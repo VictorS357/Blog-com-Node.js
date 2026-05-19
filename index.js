1ad884067c705cd6940efb97d4a3aaa7f0212862
VictorS357 <victorfransousa357@gmail.com>
2026-05-19 14:56:25 -0300

    setup express, ejs, body parser, static files, main route and server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//view engine
+app.set('view engine', 'ejs');
+
+//static
+app.use(express.static('public'));
+
+//body parser
+app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.json());
+
+//rotas
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen('8080', () => {
+  console.log('O servidor está rodando!');
+});
