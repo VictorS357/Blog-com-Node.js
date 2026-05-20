385743e635abd7392da1739794f1b0387725a304
VictorS357 <victorfransousa357@gmail.com>
2026-05-20 14:04:36 -0300

    setup connection with database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const connection = require('./database/database');
 
 //view engine
 app.set('view engine', 'ejs');
@@ -9,8 +10,17 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //body parser
-app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//database
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Conexão com database feita com sucesso!');
+  }).catch(error => {
+    console.log(error);
+  });
 
 //rotas
 app.get('/', (req, res) => {
