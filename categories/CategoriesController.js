616576b56185d00bb1399ee462b2d13af4063f8e
VictorS357 <victorfransousa357@gmail.com>
2026-05-22 16:53:46 -0300

    create category form HTML

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router(); //utiliza-se o router pois não estamos no arquivo principal (index.js)
 
-router.get('/categories', (req, res) => {
-  res.send('Rota de categorias');
-});
-
 router.get('/admin/categories/new', (req, res) => {
-  res.send('rota para criar nova categoria');
+  res.render('admin/categories/new');
 });
 
 module.exports = router;
