a5ec431bb3ecb753e5e0f760b66f773ed9e135c8
VictorS357 <victorfransousa357@gmail.com>
2026-05-29 18:50:11 -0300

    put the categories on the navbar

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ app.get('/', (req, res) => {
       ['id', 'DESC']
     ]
   }).then(articles => {
-    res.render('index', {articles});
+    Category.findAll().then(categories => {
+      res.render('index', {articles, categories});
+    });
   });
 });
 
@@ -49,7 +51,9 @@ app.get('/:slug', (req, res) => {
     where: {slug}
   }).then(article => {
     if(article != undefined) {
-      res.render('article', {article});
+      Category.findAll().then(categories => {
+        res.render('articles', {articles, categories});
+      });
     } else {
       res.redirect('/');
     }
