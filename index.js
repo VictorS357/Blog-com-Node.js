8cba4c5ac4e0c139c2feacce00d3161efff87540
VictorS357 <victorfransousa357@gmail.com>
2026-05-29 16:36:56 -0300

    create home page and fix navbar logo margin

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ app.use('/', articlesController);
 
 //rotas
 app.get('/', (req, res) => {
-  res.render('index');
+  Article.findAll().then(articles => {
+    res.render('index', {articles});
+  });
 });
 
 app.listen('8080', () => {
