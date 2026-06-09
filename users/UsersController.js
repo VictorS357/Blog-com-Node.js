77ff0558a0af438f57d252f8c574ecab0199c547
VictorS357 <victorfransousa357@gmail.com>
2026-06-09 14:32:32 -0300

    install bcrypt and save password hash on database

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User');
+const bcrypt = require('bcrypt');
 
 router.get('/admin/users', (req, res) => {
     res.send('Listagem de usuários');
@@ -14,7 +15,17 @@ router.post('/users/create', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    res.json({email, password});
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email,
+        password: hash
+    }).then(() => {
+        res.redirect('/');
+    }).catch(error => {
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
