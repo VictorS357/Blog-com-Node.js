72f288a5f708069d24fefbd0cf601c13d2d530be
VictorS357 <victorfransousa357@gmail.com>
2026-06-17 14:30:00 -0300

    testing sessions

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.set('view engine', 'ejs');
 app.use(session({
   secret: "aksndjakndkjankdssankja", cookie: {maxAge: 30000} //secret é como se fosse uma senha para decriptar suas sessões
 }))
+//o express-sessions guarda as informações das sessões no storage do servidor (utiliza memória ram do computador)
+//em sistemas de médio a grande porte, não é recomendado utilizar esse sistema de armazenamento
+//por isso, existe um banco de dados focado em salvamento de sessões, chamado Redis
 
 //static
 app.use(express.static('public'));
@@ -40,6 +43,27 @@ connection
 app.use('/', categoriesController); //utilizar rotas dentro do arquivo --- esse '/' é um prefixo (nesse caso sem prefixo) --- caso tivesse prefixo, ele seria usado sempre antes da rota princpal
 app.use('/', articlesController);
 app.use('/', usersController);
+
+//teste de sessão
+app.get('/session', (req, res) => {
+  req.session.user = {
+    username: "Victor357",
+    email: "victorfransousa357@gmail.com",
+    id: 10
+  };
+  req.session.year = 2026;
+  req.session.course = "Formação Node.js";
+
+  res.send('Sessão gerada!');
+});
+
+app.get('/read', (req, res) => {
+  res.json({
+    user: req.session.user,
+    year: req.session.year,
+    course: req.session.course
+  })
+});
 
 //rotas
 app.get('/', (req, res) => {
